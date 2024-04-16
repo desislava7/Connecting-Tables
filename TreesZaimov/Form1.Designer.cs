74a0a8adaefe4709c944eab6132adfe627e4f621
desislava7 <desislavadimitrova767@gmail.com>
2024-04-16 11:37:37 +0300

    Connection String

---
+++ TreesZaimov/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace TreesZaimov
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(55, 34);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(35, 13);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "Name";
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(26, 60);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(100, 20);
+            this.txt1.TabIndex = 1;
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Location = new System.Drawing.Point(287, 34);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(56, 13);
+            this.lbl2.TabIndex = 2;
+            this.lbl2.Text = "Name_BG";
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(262, 60);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(100, 20);
+            this.txt2.TabIndex = 3;
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(80, 119);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(247, 65);
+            this.btn1.TabIndex = 4;
+            this.btn1.Text = "Insert into razred";
+            this.btn1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.lbl1);
+            this.Name = "Form1";
+            this.Text = "Connecting Tables";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.Button btn1;
+    }
+}
+
---
+++ TreesZaimov/Form1.cs
@@ -0,0 +1,32 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TreesZaimov
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string connstr = "server = 10.6.0.127;" +
+            "port=3306;" +
+            "user=PC1;" +
+            "password= 1111;" +
+            "database=trees_zaimov";
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MySqlConnection connect = new MySqlConnection(connstr);
+            if(connect.State == 0)connect.Open();
+            MessageBox.Show("Connection now opend!");
+        }
+    }
+}
